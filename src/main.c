38d2819d16fa2d91b9581dc4f10112a7ecd722bc
maarten1C96 <12756980+maarten1C96@users.noreply.github.com>
2024-04-14 22:48:44 +0200

    feat(main.c): Display compile-time on SSD1306

--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
 // #include <stdint.h>
+#include <stdio.h>
 #include <zephyr/device.h>
 #include <zephyr/kernel.h>
 // #include <zephyr/drivers/display.h>
 #include <zephyr/display/cfb.h>
 #include <zephyr/logging/log.h>
 
-#include "logo_image.h"
+// #include "logo_image.h"
 
 #define DISPLAY_BUFFER_PITCH 128
 
@@ -25,54 +26,58 @@ void main(void)
     return;
   }
 
-  // int ret = cfb_framebuffer_init(display);
+  int ret;
+  ret = cfb_framebuffer_init(display);
+  if (ret != 0)
+  {
+    LOG_ERR("Could not initialise display");
+    return;
+  }
 
-  // if (ret != 0)
-  // {
-  //   LOG_ERR("Could not initialise display");
-  //   return;
-  // }
+  char display_buffer[128];
+  snprintf(display_buffer, sizeof(display_buffer), "%s", COMPILE_TIME);
+  ret = cfb_print(display, display_buffer, 0, 0);
+  if (ret != 0)
+  {
+    LOG_ERR("Could not print to display");
+    return;
+  }
   
-  // ret = cfb_print(display, "hello world!")
+  ret = cfb_framebuffer_finalize(display);
+  if (ret != 0)
+  {
+    LOG_ERR("Could not finalise to display");
+    return;
+  }
 
-  // int ret = cfb_framebuffer_init(display);
+  // struct display_capabilities capabilities;
+  // display_get_capabilities(display, &capabilities);
 
-  // if (ret != 0)
-  // {
-  //   LOG_ERR("Could not initialise display");
-  //   return;
-  // }
-  
-  // ret = cfb_print(display, "hello world!")
+  // const uint16_t x_res = capabilities.x_resolution;
+  // const uint16_t y_res = capabilities.y_resolution;
 
-  struct display_capabilities capabilities;
-  display_get_capabilities(display, &capabilities);
-
-  const uint16_t x_res = capabilities.x_resolution;
-  const uint16_t y_res = capabilities.y_resolution;
-
-  LOG_INF("x_resolution: %d", x_res);
-  LOG_INF("y_resolution: %d", y_res);
-  LOG_INF("supported pixel formats: %d", capabilities.supported_pixel_formats);
-  LOG_INF("screen_info: %d", capabilities.screen_info);
-  LOG_INF("current_pixel_format: %d", capabilities.current_pixel_format);
-  LOG_INF("current_orientation: %d", capabilities.current_orientation);
+  // LOG_INF("x_resolution: %d", x_res);
+  // LOG_INF("y_resolution: %d", y_res);
+  // LOG_INF("supported pixel formats: %d", capabilities.supported_pixel_formats);
+  // LOG_INF("screen_info: %d", capabilities.screen_info);
+  // LOG_INF("current_pixel_format: %d", capabilities.current_pixel_format);
+  // LOG_INF("current_orientation: %d", capabilities.current_orientation);
 	 
-  const struct display_buffer_descriptor buf_desc = {
-    .width = x_res,
-    .height = y_res,
-    .buf_size = x_res * y_res,
-    .pitch = DISPLAY_BUFFER_PITCH
-  };
+  // const struct display_buffer_descriptor buf_desc = {
+  //   .width = x_res,
+  //   .height = y_res,
+  //   .buf_size = x_res * y_res,
+  //   .pitch = DISPLAY_BUFFER_PITCH
+  // };
 
-  if (display_write(display, 0, 0, &buf_desc, buf) != 0) {
-    LOG_ERR("could not write to display");
-  }
+  // if (display_write(display, 0, 0, &buf_desc, buf) != 0) {
+  //   LOG_ERR("could not write to display");
+  // }
 
-  if (display_set_contrast(display, 0) != 0) {
-    LOG_ERR("could not set display contrast");
-  }
-  size_t ms_sleep = 5;
+  // if (display_set_contrast(display, 0) != 0) {
+  //   LOG_ERR("could not set display contrast");
+  // }
+  // size_t ms_sleep = 5;
 
   // while (true) {
   //   // Increase brightness
