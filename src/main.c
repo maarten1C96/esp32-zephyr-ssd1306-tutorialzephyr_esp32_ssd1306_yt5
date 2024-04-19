3d4e4ac28c3db626d4b35a05b027e9a05fd29078
maarten1C96 <12756980+maarten1C96@users.noreply.github.com>
2024-04-19 14:56:45 +0200

    feat: Integrate configurations from previous working `stm32-zephyr-ssd1306-tutorialzephyr_stm32_ssd1306_yt5` I²C project

--- src/main.c
+++ src/main.c
@@ -10,101 +10,45 @@
 
 #define DISPLAY_BUFFER_PITCH 128
 
-LOG_MODULE_REGISTER(display);
+LOG_MODULE_REGISTER(display_1);
 
-static const struct device *display = DEVICE_DT_GET(DT_NODELABEL(ssd1306));
+static const struct device *display_1 = DEVICE_DT_GET(DT_NODELABEL(ssd1306));
 
 void main(void)
 {
-  if (display == NULL) {
+  if (display_1 == NULL) {
     LOG_ERR("device pointer is NULL");
     return;
   }
 
-  if (!device_is_ready(display)) {
-    LOG_ERR("display device is not ready");
+  if (!device_is_ready(display_1)) {
+    LOG_ERR("display_1 device is not ready");
     return;
   }
 
   int ret;
-  ret = cfb_framebuffer_init(display);
+  ret = cfb_framebuffer_init(display_1);
   if (ret != 0)
   {
-    LOG_ERR("Could not initialise display");
+    LOG_ERR("Could not initialise display_1");
     return;
   }
 
   char display_buffer[128];
-  ret = cfb_print(display, "compiled at:", 0, 0);
+  ret = cfb_print(display_1, "compiled at:", 0, 0);
   snprintf(display_buffer, sizeof(display_buffer), "%s", COMPILE_DATE);
-  ret = cfb_print(display, display_buffer, 0, 20);
+  ret = cfb_print(display_1, display_buffer, 0, 20);
   snprintf(display_buffer, sizeof(display_buffer), "%s", COMPILE_TIME);
-  ret = cfb_print(display, display_buffer, 0, 40);
+  ret = cfb_print(display_1, display_buffer, 0, 40);
   if (ret != 0)
   {
-    LOG_ERR("Could not print to display");
+    LOG_ERR("Could not print to display_1");
     return;
   }
   
-  ret = cfb_framebuffer_finalize(display);
+  ret = cfb_framebuffer_finalize(display_1);
   if (ret != 0)
   {
-    LOG_ERR("Could not finalise to display");
+    LOG_ERR("Could not finalise to display_1");
     return;
   }
-
-  // struct display_capabilities capabilities;
-  // display_get_capabilities(display, &capabilities);
-
-  // const uint16_t x_res = capabilities.x_resolution;
-  // const uint16_t y_res = capabilities.y_resolution;
-
-  // LOG_INF("x_resolution: %d", x_res);
-  // LOG_INF("y_resolution: %d", y_res);
-  // LOG_INF("supported pixel formats: %d", capabilities.supported_pixel_formats);
-  // LOG_INF("screen_info: %d", capabilities.screen_info);
-  // LOG_INF("current_pixel_format: %d", capabilities.current_pixel_format);
-  // LOG_INF("current_orientation: %d", capabilities.current_orientation);
-	 
-  // const struct display_buffer_descriptor buf_desc = {
-  //   .width = x_res,
-  //   .height = y_res,
-  //   .buf_size = x_res * y_res,
-  //   .pitch = DISPLAY_BUFFER_PITCH
-  // };
-
-  // if (display_write(display, 0, 0, &buf_desc, buf) != 0) {
-  //   LOG_ERR("could not write to display");
-  // }
-
-  // if (display_set_contrast(display, 0) != 0) {
-  //   LOG_ERR("could not set display contrast");
-  // }
-  // size_t ms_sleep = 5;
-
-  // while (true) {
-  //   // Increase brightness
-  //   for (size_t i = 0; i < 255; i++) {
-  //     display_set_contrast(display, i);
-  //     k_sleep(K_MSEC(ms_sleep));
-  //   }
-  // while (true) {
-  //   // Increase brightness
-  //   for (size_t i = 0; i < 255; i++) {
-  //     display_set_contrast(display, i);
-  //     k_sleep(K_MSEC(ms_sleep));
-  //   }
-
-  //   // Decrease brightness
-  //   for (size_t i = 255; i > 0; i--) {
-  //     display_set_contrast(display, i);
-  //     k_sleep(K_MSEC(ms_sleep));
-  //   }
-  // }
-  //   // Decrease brightness
-  //   for (size_t i = 255; i > 0; i--) {
-  //     display_set_contrast(display, i);
-  //     k_sleep(K_MSEC(ms_sleep));
-  //   }
-  // }
-}
